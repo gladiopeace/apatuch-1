ca01553ff821a1f932e9c2d38e5b8b683649a7c0
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-15 20:16:02 +0700

    Update fly4.js

--- plugins/fly4.js
+++ plugins/fly4.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
 let isi = fs.readFileSync('src/msg/fly4.txt', 'utf8')
 let a = '```'
-conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '#menu', 'ADMIN', '#owner')
+conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
 }
 handler.help = ['fly4']
 handler.tags = ['main']
