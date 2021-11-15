6d9e20fa067516e2ef07ac03c6df11eb3e31d750
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-15 19:30:59 +0700

    Update fly2.js

--- plugins/fly2.js
+++ plugins/fly2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
 const fs = require('fs')
 let img = fs.readFileSync('tmp/IMG_20211115_045616_543.jpg')
 let isi = fs.readFileSync('src/msg/fly2.txt', 'utf8')
-conn.send2Loc(m.chat, img,  isi, global.footer, 'BACK MENU', '#menu', 'ADMIN', '#owner')
+conn.sendLoc(m.chat, img,  isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
 }
 handler.help = ['fly2']
 handler.tags = ['main']
