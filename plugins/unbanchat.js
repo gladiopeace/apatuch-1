395c282dd0497beeb6f8c2a709e2a5d56a2e0e0f
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-15 20:47:15 +0700

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -25,5 +25,6 @@ let handler = async (m, { conn, isOwner, text, isAdmin }) => {
 handler.help = ['unban']
 handler.tags = ['owner', 'group']
 handler.command = /^unban(chat)?$/i
+handler.owner = true
 handler.register = false
 module.exports = handler
