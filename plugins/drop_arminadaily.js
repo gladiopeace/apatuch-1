95fbd52615a106e4bb6db3c1ccb7e01f61b3b702
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 05:59:32 +0700

    Update drop_arminadaily.js

--- plugins/drop_arminadaily.js
+++ plugins/drop_arminadaily.js
@@ -5,7 +5,7 @@ let img = fs.readFileSync('tmp/IMG_20211115_045616_543.jpg')
 let isi = fs.readFileSync('src/msg/fly2.txt', 'utf8')
 conn.send2Loc(m.chat, img,  isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
 }
-handler.help = ['fly2']
+handler.help = ['arminadaily']
 handler.tags = ['main']
 handler.command = /^(arminadaily)$/i
 module.exports = handler
