2d686e1c24359b517a3552842e472251473eacc9
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 06:29:48 +0700

    Create drop_Subcribe_Channel_Telegram.js

---
+++ plugins/drop_Subcribe_Channel_Telegram.js
@@ -0,0 +1,13 @@
+//@amirul.dev
+let handler = async (m, { conn }) => {
+const fs = require('fs')
+let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
+let isi = fs.readFileSync('src/msg/fly4.txt', 'utf8')
+let a = '```'
+conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
+}
+handler.help = ['subtele']
+handler.tags = ['main']
+handler.command = /^(subtele)$/i
+module.exports = handler
+//@amirul.dev
