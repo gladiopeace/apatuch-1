5311b7318e13b63bb9f580ebf400df9c08390fb9
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 11:17:00 +0700

    Update drop_top_Up_ovo_gopay_dana.js

--- plugins/drop_top_Up_ovo_gopay_dana.js
+++ plugins/drop_top_Up_ovo_gopay_dana.js
@@ -1,7 +1,7 @@
 //@amirul.dev
 let handler = async (m, { conn }) => {
 const fs = require('fs')
-let img = fs.readFileSync('tmp/IMG_20211115_045616_543.jpg')
+let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
 let isi = fs.readFileSync('src/msg/topupdana.txt', 'utf8')
 conn.send2Loc(m.chat, img,  isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
 }
