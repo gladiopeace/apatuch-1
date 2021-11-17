de91decee08b9a6d7077946110f9b5ba33226b0e
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 11:34:25 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -11,9 +11,10 @@ handler.all = async function (m) {
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
     await this.sendBtn(m.chat, `
 Terimakasih telah mengirim pesan kepada saya.
-_saya akan membalasnya jika saya online_
+_saya akan membalasnya jika saya online._
+silahkan klik tombol menu untuk melihat yang saya tawarkan.
 ${user.banned ? 'kamu dibanned' : ''}
-`.trim(), '- *PESAN OTOMATIS*', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : '*menu', m)
+`.trim(), '- *PESAN OTOMATIS*\nketik #menu bila tombol tidak muncul.', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : '*menu', m)
     user.pc = new Date * 1
 }
 
