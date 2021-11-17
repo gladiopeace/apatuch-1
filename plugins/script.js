e85157187ea5778116c389dfe66f91d9eadc3fe8
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 11:28:26 +0700

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -3,22 +3,7 @@ let fs = require('fs')
 let handler = async (m, { conn }) => {
 let img = fs.readFileSync('tmp/l3.jpg')
 conn.sendLoc(m.chat, img, `*[+] SOURCE CODE [+]*
-Base Default: wabot-aq
-All Menu: No Api
-
-Script ini dibuat dari pengodean ulang wabot-aq. dikode ulang mulai dari 0, 1/1 file di buat dan dipercanggih lagi dan dibuild tanpa menggunakan api
-
-New update
-- Send Button Text
-- Send Button Image
-- Send Button Video
-- Send Button Document
-- Support Multidevice
-- Send Custom Group
-- Fake Reply Toko
-- Fake Reply Status
-- Fake Reply Contact
-- Fake Reply Button`, global.footer, 'BACK MENU', '#menu', m)
+`, global.footer, 'BACK MENU', '#menu', m)
 
 }
 
