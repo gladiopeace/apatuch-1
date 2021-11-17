ed45a3999693bcb77a825415f1a5dfdeac72f96e
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 07:01:57 +0700

    Update drop_followers_Instagram.js

--- plugins/drop_followers_Instagram.js
+++ plugins/drop_followers_Instagram.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn }) => {
 const fs = require('fs')
 let img = fs.readFileSync('tmp/IMG_20211115_045616_543.jpg')
-let isi = fs.readFileSync('src/msg/fly2.txt', 'utf8')
+let isi = fs.readFileSync('src/msg/folig.txt', 'utf8')
 conn.send2Loc(m.chat, img,  isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
 }
 handler.help = ['folig']
