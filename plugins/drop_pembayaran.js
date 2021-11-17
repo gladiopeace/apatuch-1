371fd28b3a785331aa2797b89b4b9a4c6b9fcbda
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-17 13:49:08 +0700

    Update drop_pembayaran.js

--- plugins/drop_pembayaran.js
+++ plugins/drop_pembayaran.js
@@ -4,13 +4,18 @@ const fs = require('node-fetch')
 let img = await (await fs('https://i.ibb.co/t2L1jbH/IMG-20211114-WA0008.jpg')).buffer()
 let a = '```'
 conn.send2BtnImg(m.chat, img, `🏪 *Payment E-wallet* 🏪
-🍡 DANA: 085155333010
-🍡 OVO : 085155333010
-🍡 Gopay : 085155333010
-🎲 Qris : Ada diatas
+DANA: 085155333010
+OVO : 085155333010
+Gopay : 085155333010
+Qris : Ada diatas
+
 PEMBAYARAN VIA ALFAMART
-HUBUNGI ABMIN
-(Transaksi Dibawah 10k Dana Only)
+HUBUNGI ADMIN, AGAR ADMIN BUATKAN KODE PEMBAYARANNYA
+
+TIDAK BISA MELALUI INDOMARER 
+
+( *transaksi dibawah _Rp.10.000_ wajib ke DANA* )
+
 Semua payment A/n  : 
 *M. ALAIKA ALYASAAR*`, global.footer, 'BACK MENU', '#menu', 'ADMIN', '#owner')
 }
