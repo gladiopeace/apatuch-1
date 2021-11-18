90586ed3ab7e8837c166033496a352a2e7a7a17c
bulanbintang69 <87313549+bulanbintang69@users.noreply.github.com>
2021-11-18 15:11:40 +0700

    Create drop_reseller__pubg.js

---
+++ plugins/drop_reseller__pubg.js
@@ -0,0 +1,12 @@
+//@amirul.dev
+let handler = async (m, { conn }) => {
+const fs = require('fs')
+let img = fs.readFileSync('tmp/IMG_20211115_045706_693.jpg')
+let isi = fs.readFileSync('src/msg/fly3.txt', 'utf8')
+conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner')
+}
+handler.help = ['fly3']
+handler.tags = ['main']
+handler.command = /^(respubg|ResPubg|RESPUBG)$/i
+module.exports = handler
+//@amirul.dev
