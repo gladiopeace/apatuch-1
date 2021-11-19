31913eaf73b2486311067d82e71268ac03433c9b
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-11-19 19:07:48 +0700

    Update drop_reseller__ml.js

--- plugins/drop_reseller__ml.js
+++ plugins/drop_reseller__ml.js
@@ -3,7 +3,9 @@ let handler = async (m, { conn }) => {
 const fs = require('fs')
 let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
 let isi = fs.readFileSync('src/msg/resml.txt', 'utf8')
+let format = fs.readFileSync('src/msg/formatml.txt', 'utf8')
 m.reply(isi)
+m.reply(format)
 /*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
 }
 handler.help = ['fly3']
