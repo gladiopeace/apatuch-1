e9da1cf1b7d033c760ffd59f45c8f30ebf32caf2
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-11-19 20:06:42 +0700

    Create drop_pre_order.js

---
+++ plugins/drop_pre_order.js
@@ -0,0 +1,19 @@
+//@amirul.dev
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
+let isi = fs.readFileSync('src/msg/preorder.txt', 'utf8')
+let formatml = fs.readFileSync('src/msg/tanyabarang.txt', 'utf8')
+m.reply(isi)
+m.reply(formatml)
+/*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
+
+}
+
+//handler.help = ['script']
+
+//handler.tags = ['main']
+handler.command = /^(preorder)$/i
+module.exports = handler
+//@amirul.dev
