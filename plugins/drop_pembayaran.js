b7729e5d54df434a690e24a71ebc302df3c4d5bf
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-02 23:09:46 +0700

    Update drop_pembayaran.js

--- plugins/drop_pembayaran.js
+++ plugins/drop_pembayaran.js
@@ -4,7 +4,7 @@ const fs = require('node-fetch')
 let img = await (await fs('https://i.ibb.co/t2L1jbH/IMG-20211114-WA0008.jpg')).buffer()
 
 let a = '```'
-conn.sendBtn(m.chat, img, `🏪 *Payment E-wallet* 🏪
+conn.send2BtnImg(m.chat, img, `🏪 *Payment E-wallet* 🏪
 DANA: 085155333010
 OVO : 085155333010
 Gopay : 085155333010
@@ -14,13 +14,10 @@ PEMBAYARAN VIA ALFAMART
 HUBUNGI ADMIN, AGAR ADMIN BUATKAN KODE PEMBAYARANNYA
 
 TIDAK BISA MELALUI INDOMARET 
-
-COD WILAYAH CIAMIS SEKITARNYA
-
 ( *transaksi dibawah _Rp.10.000_ wajib ke DANA* )
 
 Semua payment A/n  : 
-*M. ALAIKA ALYASAAR*`, global.footer, 'BACK MENU', '?menu', 'BACK MENU', 'g' )
+*M. ALAIKA ALYASAAR*`, global.footer, 'BACK MENU', '?menu', 'ADMIN', '?owner')
 
 }
 
