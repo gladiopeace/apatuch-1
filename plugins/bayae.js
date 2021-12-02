0a83645a6cd601b190b196e67d8ed3aa0f0f7748
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-02 22:44:12 +0700

    Update bayae.js

--- plugins/bayae.js
+++ plugins/bayae.js
@@ -1,18 +1,14 @@
 //@amirul.dev
-let fetch = require('node-fetch')
-/*let fs = require('fs')*/
 let handler = async (m, { conn }) => {
+const fs = require('node-fetch')
 let img = await (await fs('https://i.ibb.co/t2L1jbH/IMG-20211114-WA0008.jpg')).buffer()
-let isi = fs.readFileSync('src/msg/bayar.txt', 'utf8')
-conn.sendFile(m.chat, img)
-m.reply(isi)
-/*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
-
+let format = fs.readFileSync('src/msg/bayar.txt', 'utf8')
+let a = '```'
+conn.sendFile(m.chat, img, `silahkan discan`)
+m.reply(format)
 }
-
-//handler.help = ['script']
-
-//handler.tags = ['main']
+handler.help = ['fly5']
+handler.tags = ['main']
 handler.command = /^(tes)$/i
 module.exports = handler
 //@amirul.dev
