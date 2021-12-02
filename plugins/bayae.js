0025803b703ef867fdf760b027b2261f02e16b8b
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-02 22:41:46 +0700

    Update bayae.js

--- plugins/bayae.js
+++ plugins/bayae.js
@@ -1,6 +1,6 @@
 //@amirul.dev
 let fetch = require('node-fetch')
-let fs = require('fs')
+/*let fs = require('fs')*/
 let handler = async (m, { conn }) => {
 let img = await (await fs('https://i.ibb.co/t2L1jbH/IMG-20211114-WA0008.jpg')).buffer()
 let isi = fs.readFileSync('src/msg/bayar.txt', 'utf8')
