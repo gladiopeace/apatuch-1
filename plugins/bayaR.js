029c2069f4fde85a3325322d9da7e3a62fde3dec
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-02 23:21:11 +0700

    Update and rename bayae.js to bayaR.js

---
+++ plugins/bayaR.js
@@ -0,0 +1,17 @@
+//@amirul.dev
+let fetch = require('node-fetch')
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
+let isi = fs.readFileSync('src/msg/bayar.txt', 'utf8')
+m.reply(isi)
+/*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
+
+}
+
+//handler.help = ['script']
+
+//handler.tags = ['main']
+handler.command = /^(fly5)$/i
+module.exports = handler
+//@amirul.dev
