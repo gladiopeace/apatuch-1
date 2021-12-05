3bc3de01f373c4195885b290e877a7bc21358afb
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-06 03:45:09 +0700

    Update link.js

--- plugins/link.js
+++ plugins/link.js
@@ -13,14 +13,8 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['linkgroup']
 handler.tags = ['group']
 handler.command = /^link(gro?up)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
+
 
 handler.fail = null
 
