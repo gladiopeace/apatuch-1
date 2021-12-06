e0a8785e0b2d64bea7155ec3e8c24afff9a99613
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-06 16:03:59 +0700

    Update drop_ff.js

--- plugins/drop_ff.js
+++ plugins/drop_ff.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
 let isi = fs.readFileSync('src/msg/fly2.txt', 'utf8')
 let format = fs.readFileSync('src/msg/formatff.txt', 'utf8')
-m.reply(isi)
 m.reply(format)
+m.reply(isi)
 /*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
 }
 handler.help = ['fly2']
