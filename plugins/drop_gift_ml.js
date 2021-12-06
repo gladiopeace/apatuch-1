2ee14d7db1abf9a40f0ed20b745d83c96f66606e
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-06 16:04:28 +0700

    Update drop_gift_ml.js

--- plugins/drop_gift_ml.js
+++ plugins/drop_gift_ml.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn }) => {
 let img = fs.readFileSync('tmp/SAVE_20211115_044443.jpg')
 let isi = fs.readFileSync('src/msg/giftml.txt', 'utf8')
 let formatml = fs.readFileSync('src/msg/tanyabarang.txt', 'utf8')
-m.reply(isi)
 m.reply(formatml)
+m.reply(isi)
 /*conn.send2Loc(m.chat, img, isi, global.footer, 'BACK MENU', '*menu', 'ADMIN', '*owner', m)*/
 
 }
