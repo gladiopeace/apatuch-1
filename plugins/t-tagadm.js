7ff3d4db143cb3238809a086c65a867a41457501
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-12-06 15:25:55 +0700

    Update t-tagadm.js

--- plugins/t-tagadm.js
+++ plugins/t-tagadm.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, text, participants }) => {
 }
 handler.help = ['hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
-handler.command = /^(t|T)$/i
+handler.command = t
 
 handler.group = true
 handler.owner = true
